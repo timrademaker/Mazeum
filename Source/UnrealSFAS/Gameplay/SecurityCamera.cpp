958ae8e38a02ad255111b9b20bdbee6567cbc244
timrademaker <10331650+timrademaker@users.noreply.github.com>
2020-12-22 18:18:58 +0100

    Started working on security camera

---
+++ Source/UnrealSFAS/Gameplay/SecurityCamera.cpp
@@ -0,0 +1,120 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "SecurityCamera.h"
+
+#include "Components/DecalComponent.h"
+#include "Components/SplineComponent.h"
+#include "Components/StaticMeshComponent.h"
+
+#include "Kismet/GameplayStatics.h"
+#include "Kismet/KismetMathLibrary.h"
+
+// Sets default values
+ASecurityCamera::ASecurityCamera()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+	RootComponent = CreateDefaultSubobject<USceneComponent>("Root");
+	
+	PathSpline = CreateDefaultSubobject<USplineComponent>("PathSpline");
+	PathSpline->SetupAttachment(RootComponent);
+
+	CameraMesh = CreateDefaultSubobject<UStaticMeshComponent>("CameraMesh");
+	CameraMesh->SetupAttachment(RootComponent);
+
+	CameraAreaDecal = CreateDefaultSubobject<UDecalComponent>("CameraViewDecal");
+	CameraAreaDecal->SetupAttachment(RootComponent);
+
+	CameraAreaDecal->DecalSize = FVector(CameraVisionRadius);
+
+	VisionCollisionShape.SetCapsule(CameraVisionRadius, 50.0f);
+}
+
+// Called when the game starts or when spawned
+void ASecurityCamera::BeginPlay()
+{
+	Super::BeginPlay();
+
+	if (SplineEndWaitTime < 0.0f)
+	{
+		PathSpline->SetClosedLoop(true);
+	}
+
+	CameraMovementSpeed = PathSpline->Duration / SplinePathDuration;
+
+	PlayerPawn = UGameplayStatics::GetPlayerController(GetWorld(), 0)->GetPawn();
+}
+
+void ASecurityCamera::UpdateCameraTargetPosition(float DeltaTime)
+{
+	if (ReverseCountDown <= 0.0f)
+	{
+		CurrentSplineTime += CameraMovementSpeed * DeltaTime * (IsMovingBackToSplineStart ? -1.0f : 1.0f);
+		const FVector targetPosition = PathSpline->GetLocationAtTime(CurrentSplineTime, ESplineCoordinateSpace::World);
+		CameraAreaDecal->SetWorldLocation(targetPosition);
+
+		// Check if we reached the end of the spline in either direction
+		if (CurrentSplineTime >= PathSpline->Duration || CurrentSplineTime <= 0.0f)
+		{
+			if (PathSpline->IsClosedLoop())
+			{
+				// Set this back to 0, as we're not reversing along the spline.
+				CurrentSplineTime = 0.0f;
+			}
+			else
+			{
+				CurrentSplineTime = FMath::Clamp(CurrentSplineTime, 0.0f, PathSpline->Duration);
+				ReverseCountDown = SplineEndWaitTime;
+				IsMovingBackToSplineStart = !IsMovingBackToSplineStart;
+			}
+		}
+	}
+	else
+	{
+		ReverseCountDown -= DeltaTime;
+	}
+}
+
+bool ASecurityCamera::CameraHasSpottedPlayer()
+{
+	const FVector traceStart = GetActorLocation();
+	const FVector traceEnd = PathSpline->GetLocationAtTime(CurrentSplineTime, ESplineCoordinateSpace::World);
+	const FQuat traceRotaton = UKismetMathLibrary::FindLookAtRotation(traceStart, traceEnd).Quaternion();
+	FHitResult hitResult;
+
+	FCollisionQueryParams queryParams;
+	queryParams.AddIgnoredActor(this);
+
+	if (GetWorld()->SweepSingleByChannel(hitResult, traceStart, traceEnd, traceRotaton, ECollisionChannel::ECC_Camera, VisionCollisionShape, queryParams))
+	{
+		if (hitResult.GetActor() == PlayerPawn)
+		{
+			return true;
+		}
+	}
+
+	return false;
+}
+
+// Called every frame
+void ASecurityCamera::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	UpdateCameraTargetPosition(DeltaTime);
+
+	if (CameraHasSpottedPlayer())
+	{
+		// TODO: Do something
+	}
+}
+
+void ASecurityCamera::OnConstruction(const FTransform& Transform)
+{
+	Super::OnConstruction(Transform);
+
+	CameraAreaDecal->DecalSize = FVector(CameraVisionRadius);
+}
+
---
+++ Source/UnrealSFAS/Gameplay/SecurityCamera.h
@@ -0,0 +1,64 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "SecurityCamera.generated.h"
+
+class UDecalComponent;
+class USplineComponent;
+class UStaticMeshComponent;
+
+UCLASS()
+class UNREALSFAS_API ASecurityCamera : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ASecurityCamera();
+
+	// Called every frame
+	virtual void Tick(float DeltaTime) override;
+
+	virtual void OnConstruction(const FTransform& Transform) override;
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+private:
+	void UpdateCameraTargetPosition(float DeltaTime);
+	/// Returns true if the camera is able to see the player
+	bool CameraHasSpottedPlayer();
+
+public:
+	UPROPERTY(EditAnywhere, Category="Timing", meta = (ToolTip = "The time it takes for the camera to look at the spline from start to end", ClampMin = "0.1"))
+	float SplinePathDuration = 10.0f;
+	UPROPERTY(EditAnywhere, Category = "Timing", meta = (ToolTip = "The time the camera waits before reversing on its path. If negative, the camera doesn't reverse, but instead closes the spline.", ClampMin = "-1.0"))
+	float SplineEndWaitTime = 2.5f;
+	UPROPERTY(EditAnywhere, Category = "Vision", meta = (ToolTip = "The radius of the capsule which the camera uses as its vision capsule", ClampMin = "0.0"))
+	float CameraVisionRadius = 50.0f;
+	
+	UPROPERTY(EditDefaultsOnly)
+	UDecalComponent* CameraAreaDecal;
+	UPROPERTY(EditDefaultsOnly)
+	USplineComponent* PathSpline;
+	UPROPERTY(EditDefaultsOnly)
+	UStaticMeshComponent* CameraMesh;	// Will probably be changed to a different type later on, if the camera needs to rotate
+	
+
+private:
+	FCollisionShape VisionCollisionShape;
+	APawn* PlayerPawn;
+
+	/// How far along the spline we are looking
+	float CurrentSplineTime = 0.0f;
+	/// How fast the camera should move along the spline, based on spline length and SplinePathDuration
+	float CameraMovementSpeed;
+	/// True if the camera is currently moving back to the spline's start
+	bool IsMovingBackToSplineStart = false;
+	/// How long the camera still has to wait before it starts to move in the opposite direction
+	float ReverseCountDown;
+};
