3ca6dff3951f6992d0957ccf0f81bffad9776e4f
timrademaker <10331650+timrademaker@users.noreply.github.com>
2020-12-24 00:32:49 +0100

    Made it possible to make a FGuardPatrolPath struct in blueprints

--- Source/UnrealSFAS/Gameplay/GuardPatrolPath.h
+++ Source/UnrealSFAS/Gameplay/GuardPatrolPath.h
@@ -13,15 +13,15 @@ struct UNREALSFAS_API FGuardPatrolPath
 {
 	GENERATED_BODY();
 
-    UPROPERTY(EditAnywhere, Category = "Path", meta = (ToolTip = "The spline the guard follows"))
+    UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Path", meta = (ToolTip = "The spline the guard follows"))
 	USplineComponent* PathSpline;
 
-	UPROPERTY(EditAnywhere, Category = "Path", meta = (ToolTip = "The likelihood of this guard path to be chosen over another path. Higher means a bigger chance.", ClampMin = "1"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Path", meta = (ToolTip = "The likelihood of this guard path to be chosen over another path. Higher means a bigger chance.", ClampMin = "1"))
 	uint8 PathWeight = 1;
 	
-	UPROPERTY(EditAnywhere, Category = "Timing", meta = (ToolTip = "The time it takes for the guard to move from the spline's start to its end", ClampMin = "0.1"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Timing", meta = (ToolTip = "The time it takes for the guard to move from the spline's start to its end", ClampMin = "0.1"))
 	float PathDuration = 10.0f;
 	
-	UPROPERTY(EditAnywhere, Category = "Timing", meta = (ToolTip = "The time the guard waits before reversing on its path. If negative, the guard doesn't reverse, but instead closes the spline.", ClampMin = "-1.0"))
+	UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "Timing", meta = (ToolTip = "The time the guard waits before reversing on its path. If negative, the guard doesn't reverse, but instead closes the spline.", ClampMin = "-1.0"))
 	float PathEndWaitTime = 2.5f;
 };
