ce3a142c83719e5bb30317202ea6544af139af15
timrademaker <10331650+timrademaker@users.noreply.github.com>
2020-12-31 00:31:03 +0100

    Added some assets

--- Source/UnrealSFAS/ConstantsFunctionLibrary.cpp
+++ Source/UnrealSFAS/ConstantsFunctionLibrary.cpp
@@ -3,7 +3,7 @@
 
 #include "ConstantsFunctionLibrary.h"
 
-FVector UConstantsFunctionLibrary::BlockDimensions = FVector(200.0f, 200.0f, 300.0f);
+FVector UConstantsFunctionLibrary::BlockDimensions = FVector(200.0f, 200.0f, 400.0f);
 FVector UConstantsFunctionLibrary::BlockScale = FVector(1.0f);
 FVector UConstantsFunctionLibrary::VentBlockScale = FVector(2.0f, 2.0f, 1.2f);
 float UConstantsFunctionLibrary::VentHeight = 120.0f;
