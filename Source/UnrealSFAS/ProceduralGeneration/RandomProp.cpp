ba494b790c64490207dda2fd78812e20ee99f340
timrademaker <10331650+timrademaker@users.noreply.github.com>
2020-12-31 23:39:52 +0100

    Added random prop class, and added some props

---
+++ Source/UnrealSFAS/ProceduralGeneration/RandomProp.cpp
@@ -0,0 +1,57 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "RandomProp.h"
+
+// Sets default values
+ARandomProp::ARandomProp()
+{
+	PrimaryActorTick.bCanEverTick = false;
+
+	StaticMesh = CreateDefaultSubobject<UStaticMeshComponent>(TEXT("Mesh"));
+	StaticMesh->SetupAttachment(RootComponent);
+}
+
+// Called when the game starts or when spawned
+void ARandomProp::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	TArray<UStaticMesh*>* meshArrah = nullptr;
+	bool shouldBeRotated = false;
+	
+	FRotator modelRotation;
+
+	switch (PropSize)
+	{
+	case EPropSize::Small:
+	{
+		meshArrah = &SmallProps;
+		shouldBeRotated = true;
+		break;
+	}
+	case EPropSize::Medium:
+	{
+		meshArrah = &MediumProps;
+		shouldBeRotated = true;
+		break;
+	}
+	case EPropSize::Large:
+	{
+		meshArrah = &LargeProps;
+		break;
+	}
+	}
+
+	if (meshArrah->Num() > 0)
+	{
+		// Select random prop
+		UStaticMesh* meshToUse = (*meshArrah)[FMath::RandRange(0, meshArrah->Num() - 1)];
+		StaticMesh->SetStaticMesh(meshToUse);
+	}
+
+	if (shouldBeRotated)
+	{
+		SetActorRotation(FRotator(0.0f, FMath::FRandRange(0.0f, 360.0f), 0.0f));
+	}
+}
---
+++ Source/UnrealSFAS/ProceduralGeneration/RandomProp.h
@@ -0,0 +1,46 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Actor.h"
+#include "RandomProp.generated.h"
+
+UENUM()
+enum class EPropSize : uint8
+{
+	Small,
+	Medium,
+	Large
+};
+
+UCLASS()
+class UNREALSFAS_API ARandomProp : public AActor
+{
+	GENERATED_BODY()
+	
+public:	
+	// Sets default values for this actor's properties
+	ARandomProp();
+
+protected:
+	// Called when the game starts or when spawned
+	virtual void BeginPlay() override;
+
+public:
+	UPROPERTY(EditAnywhere, meta = (ToolTip = "The size of the required prop"))
+	EPropSize PropSize;
+
+	UPROPERTY(EditDefaultsOnly, Category = Meshes, meta = (ToolTip = "Props that fit on the small pedestal"))
+	TArray<UStaticMesh*> SmallProps;
+
+	UPROPERTY(EditDefaultsOnly, Category = Meshes)
+	TArray<UStaticMesh*> MediumProps;
+
+	UPROPERTY(EditDefaultsOnly, Category = Meshes, meta = (ToolTip = "Props that fit on the wide table"))
+	TArray<UStaticMesh*> LargeProps;
+
+private:
+	/** The static mesh for this prop */
+	UStaticMeshComponent* StaticMesh;
+};
